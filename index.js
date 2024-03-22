07d022616ccf62eeb283ddba4e166bc8c76990ee
Gideon Mutai <giddymutai13@gmail.com>
2024-03-22 15:30:40 +0300

    Creates functions

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 // returns a Saturday activity
-function saturdayFun(activity){
-    switch (activity) {
-        case `${activity}`:
-            return `This Saturday, I want to ${activity}!`
-        default:
-            return 'This Saturday, I want to roller-skate!'
-    }
+function saturdayFun(activity) {
+  switch (activity) {
+    case `${activity}`:
+      return `This Saturday, I want to ${activity}!`;
+    default:
+      return "This Saturday, I want to roller-skate!";
+  }
 }
 
 // returns the Monday preferred work area
-function mondayWork(workArea){
-    switch (workArea){
-        case `${workArea}`:
-            return `This Monday, I will ${workArea}.`
-        default:
-            return 'This Monday, I will go to the office.'
-    }
+function mondayWork(workArea) {
+  switch (workArea) {
+    case `${workArea}`:
+      return `This Monday, I will ${workArea}.`;
+    default:
+      return "This Monday, I will go to the office.";
+  }
 }
 
 // a function that wraps the adjective with initialized symbols
-function wrapAdjective(result="*"){
-    return function(emphatic = "a hard worker") {
-        return `You are ${result}${emphatic}${result}!`
-    }
+function wrapAdjective(result = "*") {
+  return function (emphatic = "a hard worker") {
+    return `You are ${result}${emphatic}${result}!`;
+  };
 }
 
-function wrapAdjective(result="||"){
-    return function(emphatic = "a hard worker") {
-        return `You are ${result}${emphatic}${result}!`
-    }
+function wrapAdjective(result = "||") {
+  return function (emphatic = "a hard worker") {
+    return `You are ${result}${emphatic}${result}!`;
+  };
 }
